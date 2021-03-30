85bc826023661aaa96aeb70ae8770e8850d6d5ad
rafael <rafaelcpaes@unifei.edu.br>
2021-03-30 03:11:22 -0300

    c2 - adicionando app.config

--- bot3/bot3/Program.cs
+++ bot3/bot3/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+using System.Configuration;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
-using NUnit.Framework;
 
 namespace bot3
 {
@@ -24,8 +20,8 @@ namespace bot3
             try //caso ocorra um erro 
             {
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); //espera no max 10s pra buscar o elemento
-                driver.FindElement(By.Name("username")).SendKeys("teste_teste");
-                driver.FindElement(By.Name("password")).SendKeys("testecaralho");
+                driver.FindElement(By.Name("username")).SendKeys(ConfigurationManager.AppSettings["username"]);
+                driver.FindElement(By.Name("password")).SendKeys(ConfigurationManager.AppSettings["password"]);
                 driver.FindElement(By.XPath("//*[@id='loginForm']/div/div[3]/button/div")).Click();
 
 
