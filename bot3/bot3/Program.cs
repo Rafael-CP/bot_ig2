60c2685c32cc999f97db21f3d790503614781369
rafael <rafaelcpaes@unifei.edu.br>
2021-03-30 01:24:09 -0300

    c0 - criando webdriver

---
+++ bot3/bot3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace bot3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+        }
+    }
+}
