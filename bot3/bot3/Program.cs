86eba47dc21cc255657cf3ee19d183d9b9077a15
rafael <rafaelcpaes@unifei.edu.br>
2021-03-30 02:36:18 -0300

    c1 - teste de login

--- bot3/bot3/Program.cs
+++ bot3/bot3/Program.cs
@@ -1,18 +1,40 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
-
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
 
 namespace bot3
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Navigate().GoToUrl("https://www.instagram.com/accounts/login/");
             driver.Manage().Window.Maximize();
+
+            try //caso ocorra um erro 
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); //espera no max 10s pra buscar o elemento
+                driver.FindElement(By.Name("username")).SendKeys("teste_teste");
+                driver.FindElement(By.Name("password")).SendKeys("testecaralho");
+                driver.FindElement(By.XPath("//*[@id='loginForm']/div/div[3]/button/div")).Click();
+
+
+            }
+            catch(Exception ex) //imprime o erro
+            {
+                Console.WriteLine(ex.ToString()); 
+            }
+
         }
     }
 }
