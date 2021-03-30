b0f942ea32a91cd5b2e77451b596aaa97daf7001
rafael <rafaelcpaes@unifei.edu.br>
2021-03-30 04:10:17 -0300

    c3 - bot seguindo perfil

--- bot3/bot3/Program.cs
+++ bot3/bot3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
@@ -19,16 +20,26 @@ namespace bot3
 
             try //caso ocorra um erro 
             {
+                //Thread.Sleep(TimeSpan.FromSeconds(5)); // ira literalmente esperar todos esse tempo
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); //espera no max 10s pra buscar o elemento
                 driver.FindElement(By.Name("username")).SendKeys(ConfigurationManager.AppSettings["username"]);
                 driver.FindElement(By.Name("password")).SendKeys(ConfigurationManager.AppSettings["password"]);
                 driver.FindElement(By.XPath("//*[@id='loginForm']/div/div[3]/button/div")).Click();
-
-
+                
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                driver.Navigate().GoToUrl(@"https://www.instagram.com/portalg1/");
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                driver.FindElement(By.XPath("//*[@id='react-root']/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button")).Click();
+                //driver.FindElement(By.XPath("//button[contains(text(),'Seguir')]")).Click(); // outra alternativa de XPath
             }
-            catch(Exception ex) //imprime o erro
+            catch (Exception ex) //imprime o erro
             {
                 Console.WriteLine(ex.ToString()); 
+            }
+            finally
+            {
+                driver.Close(); // fecha browser
+                driver.Dispose(); //libera memoria, etc
             }
 
         }
